1dfa6e23f5234452e58b2cb99da5da46119b89cd
Ludimila Guerra <167739772+LudimilaGuerra@users.noreply.github.com>
2025-09-02 11:05:41 -0300

    Adicionando o pacote Tarefa.service + subindo projeto

--- AppToDoList/src/main/java/ucb/aplicativo/model/Tarefas.java
+++ AppToDoList/src/main/java/ucb/aplicativo/model/Tarefas.java
@@ -4,11 +4,92 @@
  */
 package ucb.aplicativo.model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Objects;
+
 /**
  *
  * @author ludimila.guerra
  */
+
 public class Tarefas {
-    private String nome;
+    private Long id;
+    private String titulo;
     private String descricao;
+    private boolean completo;
+    private LocalDateTime dataAgora;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the dataHora
+     */
+    public LocalDateTime getDataHora() {
+        return dataAgora;
+    }
+
+    /**
+     * @param dataHora the dataHora to set
+     */
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataAgora = dataHora;
+    }
+
+    /**
+     * @return the completo
+     */
+    public boolean isCompleto() {
+        return completo;
+    }
+
+    /**
+     * @param completo the completo to set
+     */
+    public void setCompleto(boolean completo) {
+        this.completo = completo;
+    }
+    
+    
+    /*Construtor */
 }
